405edd2c6814f23af93a4d491edaa9420881f1d9
Ofarouq310 <ofarouq310@gmail.com>
2025-05-17 18:07:47 +0300

    Add guessed word component

---
+++ src/components/Word.jsx
@@ -0,0 +1,19 @@
+export default function Word (){
+    return (
+        <>
+            <section>
+                <ul className="word">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                </ul>
+            </section>
+        </>
+    )
+}
