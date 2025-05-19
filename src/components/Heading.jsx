f662f30de6795b2aecb2ac95f2aa0bf7b8b51a05
Ofarouq310 <ofarouq310@gmail.com>
2025-05-19 21:52:37 +0300

    add main heading section component

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,8 +3,13 @@ export default function Heading() {
     return(
     <>  
         <section className="header">
-            <h1>Assembly: Endgame</h1>
-            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            <div>
+                <h1>Assembly: Endgame</h1>
+                <p className="game-description">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </div>
+            <div className="message">
+               <p>"Farawell HTML & CSS"</p> 
+            </div>
         </section>
     </>
 
