f47c676f4028763ed08449e290da6499fbc0bc46
Ofarouq310 <ofarouq310@gmail.com>
2025-05-22 18:57:32 +0300

    Refactored languages component to use external language list

---
+++ src/languages.js
@@ -0,0 +1,12 @@
+const languageList = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "React",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Ruby",
+    "Assembly",
+  ];
+export default languageList;
