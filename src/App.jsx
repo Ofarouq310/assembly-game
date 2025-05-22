957fecb51e19566a0e16cb606445e8c79f6ad515
Ofarouq310 <ofarouq310@gmail.com>
2025-05-22 18:59:04 +0300

    Add game over logic to determine win/loss conditions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Heading from "./components/Heading.jsx"
 import Languages from "./components/Languages.jsx"
 import Word from "./components/Word.jsx"
 import Keyboard from './components/Keyboard.jsx'
+import languageList from './languages.js'
 import { useState } from 'react'
 
 function App() {
@@ -29,12 +30,24 @@ function App() {
     }));
   };
 
+  const gameLost = () => {
+    const lost = languageList.length - 1 === wrongGuessCount;   
+    return lost;
+  }
+
+  const gameWon = () => {
+    const won = wordLetters.every(letter => Object.keys(letterStatus).includes(letter))
+    return won;
+  }  
+
+  const gameOver = () => gameWon() || gameLost() ? true : false;
+
   return (
     <>
       <Heading />
       <Languages wrongCount={wrongGuessCount} />
       <Word word={displayWord} />
-      <Keyboard handleClick={handleKeyboardClick} letterStatus={letterStatus} />
+      <Keyboard handleClick={handleKeyboardClick} letterStatus={letterStatus} gameOver={gameOver} />
     </>
   )
 }
